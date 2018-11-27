1f908df1c7541f25e331f4d228ecad3ad53c9205
Mahmoud Adas <mido3ds@gmail.com>
2018-11-27 19:26:23 +0200

    draw team1 players

--- RetroSoccer/game.asm
+++ RetroSoccer/game.asm
@@ -1,20 +1,9 @@
 include common.inc
 
-drawPlayer proto
-drawBall proto
-drawField proto
-drawGoalKeeper proto
-
-S0X equ 44
-S1X equ 144
-S2X equ 344
-S3X equ 522
-
-R1 equ -74
-R2 equ 74
-R3 equ -167
-R4 equ -84
-R5 equ 
+drawGoalKeeper proto x:uint32,y:uint32
+drawPlayer proto x:uint32,y:uint32
+drawBall proto x:uint32,y:uint32
+drawField proto 
 
 .CODE
 fieldFileName db "assets/field.bmp",0
@@ -24,10 +13,11 @@ playersFileName db "assets/players.bmp",0
 fieldBmp Bitmap ?
 spritesheetBmp Bitmap ?
 
-s0 uint32 250
-s1 uint32 250
-s2 uint32 250
-s3 uint32 250
+stickPos uint32 250, 250, 250, 250
+
+.CONST
+PLAYER_Y dword -15, -74-15, 74-15, -167-15, -84-15, -15, 83-15, 2*83-15, 125-250-15, -15, 125-15
+STICK_X  uint32 44, 144, 344, 522
 
 .CODE
 game_asm:
@@ -39,7 +29,7 @@ onCreate proc
 	invoke loadBitmap, offset playersFileName
 	mov spritesheetBmp, eax
 
-	invoke hideMouse
+	;invoke hideMouse
 	ret
 onCreate endp
 
@@ -59,26 +49,51 @@ onUpdate endp
 ; - game rendering
 onDraw proc t:double
 	invoke drawField
-	invoke drawPlayer
-	invoke drawBall
-	invoke drawGoalKeeper
+
+	mov eax, [stickPos+0*4]
+	add eax, [PLAYER_Y+0*4]
+	invoke drawGoalKeeper, STICK_X,  eax
+
+	mov ebx, 1
+	.WHILE (ebx < 3)
+		mov eax, [stickPos+1*4]
+		add eax, [PLAYER_Y+ebx*4];y
+		invoke drawPlayer, [STICK_X+1*4], eax
+
+		inc ebx
+	.ENDW
+	.WHILE (ebx < 8)
+		mov eax, [stickPos+2*4]
+		add eax, [PLAYER_Y+ebx*4];y
+		invoke drawPlayer, [STICK_X+2*4], eax
+
+		inc ebx
+	.ENDW
+	.WHILE (ebx < 11)
+		mov eax, [stickPos+3*4]
+		add eax, [PLAYER_Y+ebx*4];y
+		invoke drawPlayer, [STICK_X+3*4], eax
+
+		inc ebx
+	.ENDW
 	ret
 onDraw endp
 
 
 BKG_CLR equ 5a5754h
 
-drawPlayer proc
-	invoke renderTBitmap, mousePos.x, mousePos.y, spritesheetBmp, 137, 31, 21, 31, BKG_CLR
+drawPlayer proc x:uint32,y:uint32
+	invoke renderTBitmap, x, y, spritesheetBmp, 137, 31, 21, 31, BKG_CLR
 	ret
 drawPlayer endp
 
-drawGoalKeeper proc
-	invoke renderTBitmap, 15, 231, spritesheetBmp, 105, 186, 21, 31, BKG_CLR
+drawGoalKeeper proc x:uint32,y:uint32
+	invoke renderTBitmap, x, y, spritesheetBmp, 105, 186, 21, 31, BKG_CLR
+	ret
 drawGoalKeeper endp
 
-drawBall proc
-	invoke renderTBitmap, 200, 200, spritesheetBmp, 198, 18, 18, 18, BKG_CLR
+drawBall proc x:uint32,y:uint32
+	invoke renderTBitmap, x, y, spritesheetBmp, 198, 18, 18, 18, BKG_CLR
 	ret
 drawBall endp
 
